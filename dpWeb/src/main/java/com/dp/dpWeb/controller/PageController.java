66a8c3da4a8df11e37c06b16254447adaa5152f8
Santanu <info.santanumaiti@gmail.com>
2018-11-01 12:11:48 +0530

    Completed Front end and other issues

---
+++ dpWeb/src/main/java/com/dp/dpWeb/controller/PageController.java
@@ -0,0 +1,55 @@
+package com.dp.dpWeb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value ={"/","/home", "/index"})
+	public ModelAndView index(){
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}
+	@RequestMapping(value ={"/about"})
+	public ModelAndView about(){
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value ={"/contact"})
+	public ModelAndView contact(){
+		ModelAndView mv= new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
+	
+}
+
+
+//=======================================================================
+//test code
+/*@RequestMapping(value ={"/test"})
+public ModelAndView test(@RequestParam(value="gretting", required=false) String greet){
+	ModelAndView mv= new ModelAndView("page");
+	mv.addObject("welcomeMsg", greet);
+	
+	return mv;
+}
+
+@RequestMapping(value ={"/test/{gretting}"})
+public ModelAndView test1(@PathVariable("gretting") String greet){
+	ModelAndView mv= new ModelAndView("page");
+	mv.addObject("welcomeMsg", greet);
+	
+	return mv;
+}*/
+
---
+++ dpWeb/src/main/webapp/WEB-INF/views/about.jsp
@@ -0,0 +1,11 @@
+<div class="container">
+	<div class="row">
+	
+		<h1>About Us</h1>
+		<hr/>
+		<p>This is a About Us Page</p>
+	
+	</div>
+
+
+</div>
